8e4ba18c2e2487e2ea6c96e96d3cd2a7405ae908
xplodwild <me@xplod.fr>
2013-11-10 17:57:29 +0100

    [1/2] InCall: Add a non-intrusive dialog for incoming calls
    Change-Id: I7c8b5cc5138a5543be30579e99bd9989624e2abe
    
    Conflicts:
    
    	AndroidManifest.xml
    	src/com/android/incallui/InCallPresenter.java

--- src/com/android/incallui/InCallPresenter.java
+++ src/com/android/incallui/InCallPresenter.java
@@ -57,6 +57,7 @@ public class InCallPresenter implements CallList.Listener {
     private Context mContext;
     private CallList mCallList;
     private InCallActivity mInCallActivity;
+    private InCallCardActivity mInCallCardActivity;
     private InCallState mInCallState = InCallState.NO_CALLS;
     private AccelerometerListener mAccelerometerListener;
     private ProximitySensor mProximitySensor;
@@ -138,12 +139,22 @@ public class InCallPresenter implements CallList.Listener {
     }
 
     private void attemptFinishActivity() {
+        // Finish our presenter card in all cases, we won't need it anymore whatever might
+        // happen.
+        if (mInCallCardActivity != null) {
+            mInCallCardActivity.finish();
+        }
+
         final boolean doFinish = (mInCallActivity != null && isActivityStarted());
         Log.i(this, "Hide in call UI: " + doFinish);
 
         if (doFinish) {
             mInCallActivity.finish();
         }
+    }
+
+    public void setCardActivity(InCallCardActivity inCallCardActivity) {
+        mInCallCardActivity = inCallCardActivity;
     }
 
     /**
@@ -677,6 +688,21 @@ public class InCallPresenter implements CallList.Listener {
             mCallUiInBackground = pm.isScreenOn() && !isKeyguardShowing;
         }
 
+        final PowerManager pm = (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
+        // If the screen is on, we'll prefer to not interrupt the user too much and slide in a card
+        if (pm.isScreenOn()) {
+            Intent intent = new Intent(mContext, InCallCardActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            mContext.startActivity(intent);
+        } else {
+            mStatusBarNotifier.updateNotificationAndLaunchIncomingCallUi(inCallState, mCallList, mCallUiInBackground);
+        }
+    }
+
+    /**
+     * Starts the incoming call Ui immediately, bypassing the card UI
+     */
+    public void startIncomingCallUi(InCallState inCallState) {
         mStatusBarNotifier.updateNotificationAndLaunchIncomingCallUi(
                 inCallState, mCallList, mCallUiInBackground);
     }
